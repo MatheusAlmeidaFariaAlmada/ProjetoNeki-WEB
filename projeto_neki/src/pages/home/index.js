61fd268fb4bf4d3b57fcf49c6657ae3fcd71fb3a
Matheus Almada <89466508+MatheusAlmeidaFariaAlmada@users.noreply.github.com>
2022-01-30 10:52:56 -0300

    tela home

--- projeto_neki/src/pages/home/index.js
+++ projeto_neki/src/pages/home/index.js
@@ -1,8 +1,50 @@
 import React,{ useState } from "react";
+import Logo from "../../assets/logo.png";
+import Lideranca from "../../assets/lideranca.jpg";
+import "./styles.css";
+
 
 function HOME() {
  return(
-     <h1>Home</h1>
+     <>
+        <div className="container">
+            <div className="container-home">
+                <div className="wrap-home">
+                    <div className="home-form">
+                    
+                        <div className="home-form-title">
+                            <img src={Logo} alt="Jovem Programador" />
+                            <span>Bem vindo a suas Skills</span>
+                        </div>
+
+                        <div>
+                            <div>
+                                <imag src={Lideranca}/>
+                            </div>
+                            <div className="skill">
+                                <div>
+                                    <span>Liderança - Level 2</span>
+                                </div>
+                        
+                            </div>
+
+                            <div className="skill">
+                                <div>
+                                    <span>Companheirismo - Level 3</span>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="container-home-form-btn">
+                            <button className="login-form-btn">Adicionar Skill</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+     </>
  )
 }
 
