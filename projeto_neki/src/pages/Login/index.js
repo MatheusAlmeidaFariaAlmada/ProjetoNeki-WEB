7a6c37f99047111d0decc92a30a308f425ce0538
Matheus <matheus.almeida.faria.almada@gmail.com>
2022-01-30 00:24:37 -0300

    vinculo api

--- projeto_neki/src/pages/Login/index.js
+++ projeto_neki/src/pages/Login/index.js
@@ -1,113 +1,120 @@
 import api from "../../services/api.js"
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { HiEye, HiEyeOff } from "react-icons/hi";
 import Logo from "../../assets/logo.png";
-import { Link, Redirect } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import "./styles.css";
 
 function LOGIN() {
 
-  const [user, setUser] = useState({
-    login: "",
-    password: ""
-  });
-  
-  const [show, setShow] = useState(false)
-  const [saveSenha, setSaveSenha] = useState(false)
+    const history = useHistory();
 
-  const handleClick = (e) => {
-    e.preventDefault()
-    setShow(!show);
-  }
+    const [user, setUser] = useState({
+        login: "",
+        password: ""
+    });
 
-  useEffect(()=>{
-    setUser({...user, password:localStorage.getItem("password"), login:localStorage.getItem("login")})
-  },[])
-  
-  function HandleLogin() {
-    if(saveSenha){
-      localStorage.setItem("password", user.password)
-      localStorage.setItem("login", user.login)
+    const [show, setShow] = useState(false)
+    const [saveSenha, setSaveSenha] = useState(false)
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        setShow(!show);
     }
-    api.post("/user/login", user).then((response)=>{
-      return <Redirect to="/home"/>
-    }).catch((err)=>{
-      console.log(err)
-      alert("Usuário não encontrado")
-    })
-  }
 
- function Save(){
-   saveSenha === true ? setSaveSenha(false) : setSaveSenha(true)
- }
+    useEffect(() => {
+        setUser({ ...user, password: localStorage.getItem("password"), login: localStorage.getItem("login") })
+    }, [])
 
-  return (
-    <div className="container">
-      <div className="container-login">
-        <div className="wrap-login">
-          <form className="login-form">
+    function HandleLogin() {
+        if (saveSenha) {
+            localStorage.setItem("password", user.password)
+            localStorage.setItem("login", user.login)
+        }
+        api.post("/user/login", user).then((response) => {
+            console.log(response)
+            history.push("/home")
+        }).catch((err) => {
+            console.log(err)
+            alert("Usuário não encontrado")
+        })
+        console.log("qualquer coisa")
+    }
 
-            <span className="login-form-title">
-              <img src={Logo} alt="Jovem Programador" />
-            </span>
+    function Save() {
+        saveSenha === true ? setSaveSenha(false) : setSaveSenha(true)
+    }
 
-            <div className="wrap-input">
-              <input
-                className={user.login !== "" ? "has-val input" : "input"}
-                type="login"
-                value={user.login}
-                onChange={(e) => setUser({ ...user, login: e.target.value })}
-              />
-              <span className="focus-input" data-placeholder="Login"></span>
+    return (
+        <>
+            <div className="container">
+                <div className="container-login">
+                    <div className="wrap-login">
+                        <div className="login-form">
+
+                            <span className="login-form-title">
+                                <img src={Logo} alt="Jovem Programador" />
+                            </span>
+
+                            <div className="wrap-input">
+                                <input
+                                    className={user.login !== "" ? "has-val input" : "input"}
+                                    type="login"
+                                    value={user.login}
+                                    onChange={(e) => setUser({ ...user, login: e.target.value })}
+                                />
+                                <span className="focus-input" data-placeholder="Login"></span>
+                            </div>
+
+                            <div className="wrap-input">
+                                <input
+                                    className={user.password !== "" ? "has-val input" : "input"}
+                                    type={show ? "text" : "password"}
+                                    value={user.password}
+                                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                                />
+                                <span className="focus-input" data-placeholder="Password"></span>
+
+                                <div className="login-eye">
+                                    {show ? (
+                                        <HiEye
+                                            size={20}
+                                            onClick={handleClick}
+                                        />
+                                    ) : (
+                                        <HiEyeOff
+                                            size={20}
+                                            onClick={handleClick}
+                                        />
+                                    )}
+                                </div>
+
+                            </div>
+
+                            <div className="checkbox">
+                                <input type="checkbox" onClick={Save} />
+                                <span>Salvar senha</span>
+                            </div>
+
+                            <div className="container-login-form-btn">
+                                <button onClick={HandleLogin} className="login-form-btn">Entrar</button>
+                            </div>
+
+
+                            <div className="text-center">
+                                <span className="txt1">Não possui conta? </span>
+                                <Link to="/cadastro">
+                                    <span className="txt2">
+                                        Criar conta
+                                    </span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="wrap-input">
-              <input
-                className={user.password !== "" ? "has-val input" : "input"}
-                type={show ? "text" : "password"}
-                value={user.password}
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-              />
-              <span className="focus-input" data-placeholder="Password"></span>
-
-              <div className="login-eye">
-                {show ? (
-                  <HiEye
-                    size={20}
-                    onClick={handleClick}
-                  />
-                ) : (
-                  <HiEyeOff
-                    size={20}
-                    onClick={handleClick}
-                  />
-                )}
-              </div>
-
-            </div>
-
-            <div className="checkbox">
-              <input type="checkbox" onClick={Save}/>
-              <span>Salvar senha</span>
-            </div>
-
-            <div className="container-login-form-btn">
-              <button onClick={HandleLogin} className="login-form-btn">Entrar</button>
-            </div>
-
-            <div className="text-center">
-              <span className="txt1">Não possui conta? </span>
-              <Link to="/cadastro">
-                <a className="txt2" href="#">
-                  Criar conta
-                </a>
-              </Link>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+        </>
+    )
 }
 
 export default LOGIN;
